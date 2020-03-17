0909c01845188c57f29002aac9b2de1568606524
Băluţoiu,Rares R.M <r.balutoiu@student.fontys.nl>
2020-03-17 22:10:44 +0100

    Schedule class

---
+++ MediaBazar/Schedule.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazar
+{
+    class Schedule
+    {
+        private int scheduleId;
+        private int employeeId;
+        private Shift shiftType;
+        private DateTime dateTime;
+        private ShiftStatus status;
+        private static int idSeeder;
+
+        public Schedule(int employeeId, Shift shiftType, DateTime dateTime)
+        {
+
+        }
+        public int ScheduleId()
+        {
+
+        }
+        public int EmployeeId()
+        {
+
+        }
+        public ShiftStatus Status()
+        {
+
+        }
+        public string ToString()
+        {
+
+        }
+
+    }
+}
